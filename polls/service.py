ef91716f818c22f3006d1cb010bbf40e97c5d48f
denis <denis3509@gmail.com>
2023-10-02 17:10:31 +0300

    polls service

---
+++ polls/service.py
@@ -0,0 +1,12 @@
+from . import models
+
+
+def create_user_test(user, poll_id):
+    user_test = models.UserTest()
+
+
+def ask_a_question(user, answers):
+    pass
+
+def get_results(user,user_test):
+    pass
